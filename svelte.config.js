655e327045c417a6071df40ebdefdf63ad6d6135
Tommi Virtanen <tv@eagain.net>
2022-12-27 11:21:30 -0700

    use @sveltejs/adapter-cloudflare@1.0.0-next.35

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { default as adapter } from "@sveltejs/adapter-cloudflare";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
