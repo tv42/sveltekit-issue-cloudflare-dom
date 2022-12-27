94d026ab4783d154d19b3f07228c8bb96743f3bc
Tommi Virtanen <tv@eagain.net>
2022-12-27 11:22:20 -0700

    Add simple DOM manipulation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    const h1 = document.getElementById("demo");
+    if (h1) {
+      const span = document.createElement('span');
+      span.innerText = "dynamic content";
+      h1.append(span);
+	}
+  });
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p id="demo">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
